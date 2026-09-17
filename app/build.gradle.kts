f6d9bdb972ef9ae0b6d449660707f77ee973ee0d
yash410212 <yash4102122@gmail.com>
2026-09-17 18:37:31 +0530

    Add Android app Gradle config

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.scopespark.chargescope"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.scopespark.chargescope"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
